13d535c1b00b3c00bfc8b8fe031f177056b1c07f
Rodrigo Sanguanini <rodrigokiller@gmail.com>
2017-06-30 08:38:22 -0300

    'Nothing'

--- rh.py
+++ rh.py
@@ -1,31 +1,75 @@
-import asyncio
-import random
-import re
-import sys
-import time
-import traceback
-from datetime import timedelta, datetime
-
 import discord
-import psutil
 from discord.ext import commands
+import random
 
-from config import *
-from utils import checks
-from utils.database import init_database, userDatabase, reload_worlds, tracked_worlds, tracked_worlds_list, \
-    reload_welcome_messages, welcome_messages, reload_announce_channels
-from utils.discord import get_member, send_log_message, get_region_string, get_channel_by_name, get_user_servers, \
-    clean_string, get_role_list, get_member_by_name, get_announce_channel, get_user_worlds
-from utils.general import command_list, join_list, get_uptime, TimeString, \
-    single_line, is_numeric, getLogin, start_time, global_online_list
-from utils.general import log
-from utils.help_format import NabHelpFormat
-from utils.messages import decode_emoji, deathmessages_player, deathmessages_monster, EMOJI, levelmessages, \
-    weighedChoice, formatMessage
-from utils.tibia import get_server_online, get_character, ERROR_NETWORK, ERROR_DOESNTEXIST, \
-    get_voc_abb, get_highscores, tibia_worlds, get_pronouns, parse_tibia_time, get_voc_emoji
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
 
-description = '''Mission: Destroy all humans.'''
-bot = commands.Bot(command_prefix=["/"], description=description, pm_help=True, formatter=NabHelpFormat())
-# We remove the default help command so we can override it
-bot.remove_command("help")
+There are a number of utility commands being showcased here.'''
+bot = commands.Bot(command_prefix='?', description=description)
+
+@bot.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await bot.send_message(message.channel, msg)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def add(left : int, right : int):
+    """Adds two numbers together."""
+    await bot.say(left + right)
+
+@bot.command()
+async def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(*choices : str):
+    """Chooses between multiple choices."""
+    await bot.say(random.choice(choices))
+
+@bot.command()
+async def repeat(times : int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await bot.say(content)
+
+@bot.command()
+async def joined(member : discord.Member):
+    """Says when a member joined."""
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
+@bot.group(pass_context=True)
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+@cool.command(name='bot')
+async def _bot():
+    """Is the bot cool?"""
+    await bot.say('Yes, the bot is cool.')
+
+bot.run('MjkwMjI2MTc4NTcxMTA4MzU1.DDXrxQ.b5JorMgQmRPOaBDRki0G5bEbbfo')
