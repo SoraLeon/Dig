2b0dc6889bdb619e841d45d003b14e88d84de172
Rodrigo Sanguanini <rodrigokiller@gmail.com>
2017-07-09 18:25:48 -0300

    Ajustes para mostrar em lista os comandos do pobre
    Ao pesquisar itens do pobre, retorna lista com ID, ao informar o número da lista, ele retorna os detalhes

--- rh_pobre.py
+++ rh_pobre.py
@@ -43,9 +43,42 @@ class Pobre:
             return
         long = ctx.message.channel.is_private or ctx.message.channel.name == ask_channel_name
         embed = self.get_tool_embed(ctx, tool, long)
-        if len(tool) > 1:                        
-            yield from self.bot.say("Eu trouxe uma lista que bate com o termo pesquisado, mas se me passar o nome mais detalhado, posso trazer mais detalhes.", embed=embed)
-        else:
+        if len(tool) > 1: 
+            start = 0
+            answer = '+'
+            bot_message = yield from self.bot.say("Escolha uma das opções abaixo (0: Cancelar):", embed=embed)
+            while answer == '+' or answer == '-':                
+                answer = yield from self.bot.wait_for_message(author=ctx.message.author, channel=ctx.message.channel,timeout=30.0)
+                if answer is None:
+                    yield from self.bot.say("... opa, esqueceu de mim? Beleza, então!")
+                    return                    
+                elif is_numeric(answer.content):
+                    answer = int(answer.content)
+                    if answer == 0:
+                        yield from self.bot.say("Mudou de ideia? Típico.")
+                        return                                      
+                    try:    
+                        choice = tool[answer-1]
+                        tool = []             
+                        details = yield from get_details(choice["link"], "26", '0')
+                        tool.append({'name' : choice["name"], 'link' : choice["link"], 'details': details})
+                        embed = self.get_tool_embed(ctx, tool, long) 
+                    except IndexError:
+                        yield from self.bot.say("Nossa, pra que fazer isso, você nem escolheu algo válido. Agora comece de novo.")
+                elif isinstance(answer.content, str) and (answer.content == '-' or answer.content == '+'):
+                    if not ctx.message.channel.is_private:
+                        yield from self.bot.delete_message(answer)
+                    answer = str(answer.content)
+                    if  answer == '-' and start - 10 >= 0:
+                        start -= 10
+                        embed = self.get_tool_embed(ctx, tool, long, start) 
+                    elif answer == '+' and start + 10 < len(tool):
+                        start += 10
+                        embed = self.get_tool_embed(ctx, tool, long, start)                        
+                    yield from self.bot.edit_message(bot_message, "Escolha uma das opções abaixo (0: Cancelar):", embed=embed)     
+                else:
+                    yield from self.bot.say("Essa não é uma resposta válida!")
+        if len(tool) == 1:
             yield from self.bot.say(embed=embed)
 
             # Attach item's image only if the bot has permissions
@@ -81,17 +114,49 @@ class Pobre:
         if name is None:
             yield from self.bot.say("Diga-me o nome da tradução que desejas pesquisar.")
             return
-        traducao = yield from get_search(parse.quote(name.replace(' ', '+').encode('iso-8859-1'), safe='+'), "23", '0')
+        traducao = yield from get_search(parse.quote(name.replace(' ', '+').encode('iso-8859-1'), safe='+'), "23", '0')        
         if traducao is None or len(traducao) == 0:
             yield from self.bot.say("Não encontrei nenhuma tradução.")
             return
         long = ctx.message.channel.is_private or ctx.message.channel.name == ask_channel_name
         embed = self.get_traducao_embed(ctx, traducao, long)
-        if len(traducao) > 1:                        
-            yield from self.bot.say("Não pude trazer os detalhes desta tradução, mas eu trouxe uma lista com possíveis resultados:", embed=embed)
-        else:
+        if len(traducao) > 1:  
+            start = 0
+            answer = '+'
+            yield from self.bot.say("Escolha uma das opções abaixo (0: Cancelar):", embed=embed)
+            while answer == '+' or answer == '-':                                      
+                answer = yield from self.bot.wait_for_message(author=ctx.message.author, channel=ctx.message.channel,timeout=30.0)
+                if answer is None:
+                    yield from self.bot.say("... opa, esqueceu de mim? Beleza, então!")
+                    return
+                elif is_numeric(answer.content):
+                    answer = int(answer.content)
+                    if answer == 0:
+                        yield from self.bot.say("Mudou de ideia? Típico.")
+                        return
+                    try:    
+                        tchoicerd = traducao[answer-1]
+                        traducao = []             
+                        details = yield from get_details(choice["link"], "23", '0')
+                        traducao.append({'name' : choice["name"], 'link' : choice["link"], 'details': details})
+                        embed = self.get_traducao_embed(ctx, traducao, long) 
+                    except IndexError:
+                        yield from self.bot.say("Nossa, pra que fazer isso, você nem escolheu algo válido. Agora comece de novo.")
+                elif isinstance(answer.content, str) and (answer.content == '-' or answer.content == '+'):
+                    if not ctx.message.channel.is_private:
+                        yield from self.bot.delete_message(answer)
+                    answer = str(answer.content)
+                    if  answer == '-' and start - 10 >= 0:
+                        start -= 10
+                        embed = self.get_traducao_embed(ctx, traducao, long, start) 
+                    elif answer == '+' and start + 10 < len(tool):
+                        start += 10
+                        embed = self.get_traducao_embed(ctx, traducao, long, start)
+                    yield from self.bot.edit_message(bot_message, "Escolha uma das opções abaixo (0: Cancelar):", embed=embed)     
+                else:
+                    yield from self.bot.say("Essa não é uma resposta válida!")
+        if len(traducao) == 1:
             yield from self.bot.say(embed=embed) 
-
             # Attach item's image only if the bot has permissions
             permissions = ctx.message.channel.permissions_for(get_member(self.bot, self.bot.user.id, ctx.message.server))
             trad = traducao[0]
@@ -132,11 +197,43 @@ class Pobre:
             return
         long = ctx.message.channel.is_private or ctx.message.channel.name == ask_channel_name
         embed = self.get_tutorial_embed(ctx, tutorial, long)
-        if len(tutorial) > 1:                        
-            yield from self.bot.say("Não pude trazer os detalhes deste tutorial, mas eu trouxe uma lista com possíveis resultados:", embed=embed)
-        else:
+        if len(tutorial) > 1:  
+            start = 0
+            answer = '+'
+            yield from self.bot.say("Escolha uma das opções abaixo (0: Cancelar):", embed=embed)
+            while answer == '+' or answer == '-':                                       
+                answer = yield from self.bot.wait_for_message(author=ctx.message.author, channel=ctx.message.channel,timeout=30.0)
+                if answer is None:
+                    yield from self.bot.say("... opa, esqueceu de mim? Beleza, então!")
+                    return
+                elif is_numeric(answer.content):
+                    answer = int(answer.content)
+                    if answer == 0:
+                        yield from self.bot.say("Mudou de ideia? Típico.")
+                        return
+                    try:    
+                        choice = tutorial[answer-1]
+                        tutorial = []             
+                        details = yield from get_details(choice["link"], "19", '0')
+                        tutorial.append({'name' : choice["name"], 'link' : choice["link"], 'details': details})
+                        embed = self.get_tutorial_embed(ctx, tutorial, long) 
+                    except IndexError:
+                        yield from self.bot.say("Nossa, pra que fazer isso, você nem escolheu algo válido. Agora comece de novo.")
+                elif isinstance(answer.content, str) and (answer.content == '-' or answer.content == '+'):
+                    if not ctx.message.channel.is_private:
+                        yield from self.bot.delete_message(answer)
+                    answer = str(answer.content)
+                    if  answer == '-' and start - 10 >= 0:
+                        start -= 10
+                        embed = self.get_tutorial_embed(ctx, tutorial, long, start) 
+                    elif answer == '+' and start + 10 < len(tool):
+                        start += 10
+                        embed = self.get_tutorial_embed(ctx, tutorial, long, start)
+                    yield from self.bot.edit_message(bot_message, "Escolha uma das opções abaixo (0: Cancelar):", embed=embed)     
+                else:
+                    yield from self.bot.say("Essa não é uma resposta válida!")
+        if len(tutorial) == 1:
             yield from self.bot.say(embed=embed)  
-
             # Attach item's image only if the bot has permissions
             permissions = ctx.message.channel.permissions_for(get_member(self.bot, self.bot.user.id, ctx.message.server))
             trad = tutorial[0]
@@ -158,18 +255,17 @@ class Pobre:
                             os.remove(filename)      
 
     @staticmethod
-    def get_tool_embed(ctx, tool, long):
+    def get_tool_embed(ctx, tool, long, start=0):
         """Gets the tool embed to show in /utilitario command"""
-        short_limit = 5
-        long_limit = 40
-
         title = "Ferramentas"
 
         desc = ""
-        for i in tool[:10]:
-            desc += "[{0}]({1})\n".format(i["name"], i["link"])
-
+        index = start
+        for i in tool[start:start+10]:
+            # Se for apenas um resultado, já trás todos os dados            
             if "details" in i and len(i["details"]) > 0:
+                if "name" in i:
+                    desc += "**Nome:** [{0}]({1})\n".format(i["name"], i["link"])
                 if "autor" in i["details"]:
                     desc += "**Autor:** {0}\n".format(i["details"]["autor"])
                 if "sistema" in i["details"]:    
@@ -183,23 +279,31 @@ class Pobre:
                 if "descricao" in i["details"]:
                     desc += "**Descrição:** {0}\n".format(i["details"]["descricao"])              
                 title = "Detalhes da ferramenta"          
+            # Senão retorna a lista contendo o link para cada um e um número     
+            else:
+                desc += "{2}: [{0}]({1})\n".format(i["name"], i["link"], index+1)
+            # Lista informações de próxima página já que o embed contém um limite máximo de caracteres
+            index += 1    
+            if index == start+10 and start >= 10:
+                desc += "*-: Página anterior*\t"
+            if index == start+10 and len(tool) > start+10:                
+                desc += "*+: Próxima página*\n"
         
         embed = discord.Embed(title=title, description=desc)      
         return embed            
 
     @staticmethod
-    def get_traducao_embed(ctx, traducao, long):
+    def get_traducao_embed(ctx, traducao, long, start=0):
         """Gets the tool embed to show in /traducao command"""
-        short_limit = 5
-        long_limit = 40
-
         title = "Traduções"
 
         desc = ""
-        for i in traducao[:10]:
-            desc += "[{0}]({1})\n".format(i["name"], i["link"]) 
-
+        index = start
+        for i in traducao[start:start+10]:
+            # Se for apenas um resultado, já trás todos os dados 
             if "details" in i and len(i["details"]) > 0:
+                if "name" in i:
+                    desc += "**Nome:** [{0}]({1})\n".format(i["name"], i["link"])
                 if "sistema" in i["details"]:    
                     desc += "**Sistema:** {0}\n".format(i["details"]["sistema"])
                 if "autor" in i["details"]:
@@ -220,24 +324,32 @@ class Pobre:
                     desc += "**Descrição:** {0}\n".format(i["details"]["descricao"])  
                 if "consideracoes" in i["details"]:
                     desc += "**Considerações:** {0}\n".format(i["details"]["consideracoes"])                  
-                title = i["name"]    
-        
+                title = "Detalhes da tradução"
+            # Senão retorna a lista contendo o link para cada um e um número       
+            else:     
+                desc += "{2}: [{0}]({1})\n".format(i["name"], i["link"], index+1)
+            # Lista informações de próxima página já que o embed contém um limite máximo de caracteres    
+            index += 1
+            if index == start+10 and start >= 10:
+                desc += "*-: Página anterior*\t"
+            if index == start+10 and len(tool) > start+10:                
+                desc += "*+: Próxima página*\n"
+
         embed = discord.Embed(title=title, description=desc)      
         return embed   
 
     @staticmethod
-    def get_tutorial_embed(ctx, tutorial, long):
+    def get_tutorial_embed(ctx, tutorial, long, start=0):
         """Gets the tool embed to show in /tutorial command"""
-        short_limit = 5
-        long_limit = 40
-
         title = "Tutoriais"
 
         desc = ""
-        for i in tutorial[:10]:
-            desc += "[{0}]({1})\n".format(i["name"], i["link"])   
-
+        index = start
+        for i in tutorial[start:start+10]:     
+            # Se for apenas um resultado, já trás todos os dados         
             if "details" in i and len(i["details"]) > 0:
+                if "name" in i:
+                    desc += "**Nome:** [{0}]({1})\n".format(i["name"], i["link"])
                 if "sistema" in i["details"]:    
                     desc += "**Sistema:** {0}\n".format(i["details"]["sistema"])
                 if "autor" in i["details"]:
@@ -258,7 +370,16 @@ class Pobre:
                     desc += "**Descrição:** {0}\n".format(i["details"]["descricao"])  
                 if "consideracoes" in i["details"]:
                     desc += "**Considerações:** {0}\n".format(i["details"]["consideracoes"])                  
-                title = i["name"]  
+                title = "Detalhes do documento" 
+            # Senão retorna a lista contendo o link para cada um e um número      
+            else:
+                desc += "{2}: [{0}]({1})\n".format(i["name"], i["link"], index+1)  
+            # Lista informações de próxima página já que o embed contém um limite máximo de caracteres        
+            index += 1
+            if index == start+10 and start >= 10:
+                desc += "*-: Página anterior*\t"
+            if index == start+10 and len(tool) > start+10:                
+                desc += "*+: Próxima página*\n"
         
         embed = discord.Embed(title=title, description=desc)      
         return embed            
